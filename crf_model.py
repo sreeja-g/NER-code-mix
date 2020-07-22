20e784d7b0946b89a74a9684dd6386ee937417d1
sreeja-g <37956427+sreeja-g@users.noreply.github.com>
2020-07-23 04:28:18 +0530

    lstm_modeel

--- crf_model.py
+++ crf_model.py
@@ -1,14 +1,5 @@
 from sklearn_crfsuite import CRF
-from sklearn.model_selection import cross_val_predict
 from sklearn_crfsuite.metrics import flat_classification_report, flat_f1_score
-from sklearn.metrics import make_scorer
-from sklearn.metrics import f1_score
-from sklearn.model_selection import RandomizedSearchCV
-
-import sklearn_crfsuite
-from sklearn_crfsuite import scorers
-
-import scipy.stats
 
 import featureVec
 
@@ -46,27 +37,3 @@ sorted_labels = sorted(
 print(flat_classification_report(
     y_test, y_pred, labels=sorted_labels, digits=3
 ))
-
-# crf = sklearn_crfsuite.CRF(
-#     algorithm='l2sgd',
-#     max_iterations=100,
-#     all_possible_transitions=True
-# )
-# params_space = {
-#     'c2': scipy.stats.expon(scale=0.05),
-# }
-
-# f1_scorer = make_scorer(flat_f1_score,
-#                         average='weighted', labels=labels)
-
-# rs = RandomizedSearchCV(crf, params_space,
-#                         cv=3,
-#                         verbose=1,
-#                         n_jobs=-1,
-#                         n_iter=50,
-#                         scoring=f1_scorer)
-# rs.fit(X_train, y_train)
-
-# print('best params:', rs.best_params_)
-# print('best CV score:', rs.best_score_)
-# print('model size: {:0.2f}M'.format(rs.best_estimator_.size_ / 1000000))
--- lstm_model.py
+++ lstm_model.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 
 from keras.models import Sequential
@@ -5,22 +6,41 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 
-dataset = read_csv('processed_data/featureVec.csv', header=0)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+
+dataset = pd.read_csv('processed_data/featureVec.csv', header=0)
 val = dataset.values
 val=val.astype('float32')
 val = np.nan_to_num(val)
 
 X = val[:,:32]
-Y = val[:,32]
+y = val[:,32]
 
 
 X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 model = Sequential()
 model.add(LSTM(100, input_shape=(32, 1)))
 model.add(Dropout(0.3))
 model.add(Dense(7,activation='softmax')) #7 class classification.
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
-model.fit(X, Y, epochs=5, batch_size=32, validation_split = 0.2, verbose=1)
+model.fit(X_train, y_train, epochs=5, batch_size=32, validation_split = 0.2, verbose=1)
 
 model.summary()
+
+y_pred = model.predict(X_test)
+
+y_pred = np.argmax(y_pred, axis=1)
+
+
+print("Results for LSTM..")
+
+print(classification_report(y_test, y_pred, target_names=target_names))
+
+
+score = f1_score(y_pred, y_test, average='weighted')
+print("LSTM F1 score: {:.2f}".format(score))
